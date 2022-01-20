5299c1eb975ada65a508c5f94751028a6306188b
Ad3bay0c <aclintonadebayo@gmail.com>
2022-01-20 22:56:58 +0100

    Implemented the customer interface implicitly and creates an initializer

--- domain/customerRepositoryStub.go
+++ domain/customerRepositoryStub.go
@@ -1,1 +1,34 @@
 package domain
+
+type CustomerRepositoryStub struct {
+	customers []Customer
+}
+
+func (c CustomerRepositoryStub) FindAll() ([]Customer, error)  {
+	return c.customers, nil
+}
+
+func NewCustomerRepositoryStub() CustomerRepositoryStub {
+	customers := []Customer{
+		{
+			ID:      "1",
+			Name:    "John Doe",
+			City:    "New York",
+			Zipcode: "10001",
+			Dob:     "01/01/1990",
+			Status:  "active",
+		},
+		{
+			ID:      "2",
+			Name:    "Jane Doe",
+			City:    "New York",
+			Zipcode: "10001",
+			Dob:     "01/01/1990",
+			Status:  "active",
+		},
+	}
+
+	return CustomerRepositoryStub{
+		customers: customers,
+	}
+}
