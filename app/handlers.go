44e28c9850e08194345beccc75c1ff53c274ad5d
Ad3bay0c <aclintonadebayo@gmail.com>
2022-01-20 22:22:09 +0100

    Contains the greet and create handlers

---
+++ app/handlers.go
@@ -0,0 +1,17 @@
+package app
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func Create(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func Greet(w http.ResponseWriter, req *http.Request) {
+    params := mux.Vars(req)
+
+	fmt.Fprintf(w,"%v", params["id"])
+}
