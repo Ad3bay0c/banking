b87e728c397675af766e502181b6211eae66721c
Ad3bay0c <aclintonadebayo@gmail.com>
2022-01-20 23:39:29 +0100

    Separated the routers and carefully set up the server so that it shuts down gracefully

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "github.com/Ad3bay0c/banking/app"
+import (
+	"github.com/Ad3bay0c/banking/app"
+	"github.com/joho/godotenv"
+	"log"
+)
 
 func main() {
+	log.SetFlags(log.Llongfile|log.Llongfile|log.Ldate|log.Ltime)
+	if err := godotenv.Load(); err != nil {
+		log.Printf("Error reading .env file\n")
+	}
 	app.Start()
 }
