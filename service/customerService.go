f98a3c7752ef2936fdab28785ebdb8a9f42f07e9
Ad3bay0c <aclintonadebayo@gmail.com>
2022-01-20 22:57:44 +0100

    created the primary ports (i.e. CustomerService interface)

--- service/customerService.go
+++ service/customerService.go
@@ -1,1 +1,19 @@
 package service
+
+import "github.com/Ad3bay0c/banking/domain"
+
+type CustomerService interface {
+	GetAllCustomers() ([]domain.Customer, error)
+}
+
+type DefaultCustomerService struct {
+	repo domain.CustomerRepository
+}
+
+func (d DefaultCustomerService) GetAllCustomers() ([]domain.Customer, error)  {
+	return d.repo.FindAll()
+}
+
+func NewCustomerService(repository domain.CustomerRepository) DefaultCustomerService  {
+	return DefaultCustomerService{repo: repository}
+}
