3dacdbece6610f0b79eaa2e42bc30081b0c9be15
Ad3bay0c <aclintonadebayo@gmail.com>
2022-01-21 20:18:19 +0100

    added struct tags to the Customer struct

--- domain/customer.go
+++ domain/customer.go
@@ -3,15 +3,16 @@ package domain
 import "github.com/Ad3bay0c/banking/errs"
 
 type Customer struct {
-	ID      string
-	Name    string
-	City    string
-	Zipcode string
-	Dob     string
-	Status  string
+	ID      string	`json:"customer_id" db:"customer_id"`
+	Name    string	`json:"name"`
+	City    string	`json:"city"`
+	Zipcode string	`json:"zipcode"`
+	Dob     string	`json:"date_of_birth" db:"date_of_birth"`
+	Status  string	`json:"status"`
 }
 
 type CustomerRepository interface {
-	FindAll() ([]Customer, error)
+	// FindAll status can be 1: active, 2: inactive
+	FindAll(status string) ([]Customer, *errs.AppError)
 	ByID(id string) (*Customer, *errs.AppError)
 }
