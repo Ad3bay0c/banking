717785e1097dee6590b3a443c45fca31309b0fd3
Ad3bay0c <aclintonadebayo@gmail.com>
2022-01-22 22:12:52 +0100

    created a transacton request data transfer object

--- dto/transactionRequest.go
+++ dto/transactionRequest.go
@@ -1,1 +1,29 @@
 package dto
+
+import (
+	"github.com/Ad3bay0c/banking/errs"
+	"strings"
+)
+
+const (
+	WITHDRAW = "withdrawal"
+	DEPOSIT = "deposit"
+)
+type TransactionRequest struct {
+	TransactionID        string `json:"id"`
+	AccountID			 string `json:"account_id"`
+	Amount				 float64 `json:"amount"`
+	TransactionType		 string `json:"transaction_type"`
+	TransactionDate      string `json:"transaction_date"`
+}
+
+func (req TransactionRequest) Validate() *errs.AppError {
+	if strings.ToLower(req.TransactionType) != WITHDRAW && strings.ToLower(req.TransactionType) != DEPOSIT {
+		return errs.NewValidationError("transaction type can either by withdrawal or deposit")
+	}
+	return nil
+}
+
+func (req TransactionRequest) IsTransactionWithdrawal() bool {
+	return req.TransactionType == WITHDRAW
+}
