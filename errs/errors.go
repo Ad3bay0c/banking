61f0fc193dc2ce70bcf34393e5bfbaeb36fc5af8
Ad3bay0c <aclintonadebayo@gmail.com>
2022-01-22 10:52:22 +0100

    Go formatter applied

--- errs/errors.go
+++ errs/errors.go
@@ -3,20 +3,20 @@ package errs
 import "net/http"
 
 type AppError struct {
-	Code 	int
+	Code    int
 	Message string
 }
 
 func NewNotFoundError(message string) *AppError {
-    return &AppError{
-        Code: http.StatusNotFound,
-        Message: message,
-    }
+	return &AppError{
+		Code:    http.StatusNotFound,
+		Message: message,
+	}
 }
 
 func NewUnexpectedError(message string) *AppError {
-    return &AppError{
-        Code: http.StatusInternalServerError,
-        Message: message,
-    }
+	return &AppError{
+		Code:    http.StatusInternalServerError,
+		Message: message,
+	}
 }
