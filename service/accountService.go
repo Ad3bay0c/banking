7c73c23e4cb4808ab853b505944002b0647ab91e
Ad3bay0c <aclintonadebayo@gmail.com>
2022-01-22 10:55:37 +0100

    created an interface containing Save method

--- service/accountService.go
+++ service/accountService.go
@@ -1,1 +1,15 @@
 package service
+
+import (
+	"github.com/Ad3bay0c/banking/domain/account"
+	"github.com/Ad3bay0c/banking/errs"
+)
+
+type AccountService interface {
+	NewAccount() *errs.AppError
+}
+
+type DefaultAccountService struct {
+	repo account.Repository
+}
+
