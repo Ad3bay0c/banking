e194a92fcb8775fce8ce1fceb7d0e9187f096ed4
Ad3bay0c <aclintonadebayo@gmail.com>
2022-01-22 22:13:07 +0100

    created a transacton response data transfer object

--- dto/transactionResponse.go
+++ dto/transactionResponse.go
@@ -1,1 +1,9 @@
 package dto
+
+type TransactionResponse struct {
+	TransactionID        string `json:"id"`
+	AccountID			 string `json:"account_id"`
+	Amount				 float64 `json:"amount"`
+	TransactionType		 string `json:"transaction_type"`
+	TransactionDate      string `json:"transaction_date"`
+}
