2212392b3a6db1c46d531f46cc134a60a3c956e7
Ad3bay0c <aclintonadebayo@gmail.com>
2022-01-22 22:12:24 +0100

    added transaction model

--- domain/account/transaction.go
+++ domain/account/transaction.go
@@ -1,1 +1,28 @@
 package account
+
+import (
+	"github.com/Ad3bay0c/banking/dto"
+	"strings"
+)
+
+type Transaction struct {
+	TransactionID        string `json:"id"`
+	AccountID			 string `json:"account_id"`
+	Amount				 float64 `json:"amount"`
+	TransactionType		 string `json:"transaction_type"`
+	TransactionDate      string `json:"transaction_date"`
+}
+
+func (transact Transaction) ToNewTransactionResponseDto() dto.TransactionResponse {
+	return dto.TransactionResponse{
+		TransactionID:   transact.TransactionID,
+		AccountID:       transact.AccountID,
+		Amount:          transact.Amount,
+		TransactionType: transact.TransactionType,
+		TransactionDate: transact.TransactionDate,
+	}
+}
+
+func (transact *Transaction) IsWithdrawal() bool {
+	return strings.ToLower(transact.TransactionType) == dto.WITHDRAW
+}
