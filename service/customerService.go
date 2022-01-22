4cec34fdf77c1a56bd25886158f87123e9cb5318
Ad3bay0c <aclintonadebayo@gmail.com>
2022-01-22 10:56:26 +0100

    changed return value to dto response

--- service/customerService.go
+++ service/customerService.go
@@ -2,23 +2,24 @@ package service
 
 import (
 	"github.com/Ad3bay0c/banking/domain"
+	"github.com/Ad3bay0c/banking/dto"
 	"github.com/Ad3bay0c/banking/errs"
 )
 
 type CustomerService interface {
 	GetAllCustomers(status string) ([]domain.Customer, *errs.AppError)
-	GetCustomerByID(id string) (*domain.Customer, *errs.AppError)
+	GetCustomerByID(id string) (*dto.CustomerResponse, *errs.AppError)
 }
 
 type DefaultCustomerService struct {
 	repo domain.CustomerRepository
 }
 
-func NewCustomerService(repository domain.CustomerRepository) DefaultCustomerService  {
+func NewCustomerService(repository domain.CustomerRepository) DefaultCustomerService {
 	return DefaultCustomerService{repo: repository}
 }
 
-func (d DefaultCustomerService) GetAllCustomers(status string) ([]domain.Customer, *errs.AppError)  {
+func (d DefaultCustomerService) GetAllCustomers(status string) ([]domain.Customer, *errs.AppError) {
 	if status == "active" {
 		status = "1"
 	} else if status == "inactive" {
@@ -29,6 +30,10 @@ func (d DefaultCustomerService) GetAllCustomers(status string) ([]domain.Custome
 	return d.repo.FindAll(status)
 }
 
-func (d DefaultCustomerService) GetCustomerByID(id string) (*domain.Customer, *errs.AppError)  {
-	return d.repo.ByID(id)
+func (d DefaultCustomerService) GetCustomerByID(id string) (*dto.CustomerResponse, *errs.AppError) {
+	c, err := d.repo.ByID(id)
+	if err != nil {
+		return nil, err
+	}
+	return c.ToDto(), nil
 }
