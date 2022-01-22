86651882128e9dfb157115b38827e251f7f53146
Ad3bay0c <aclintonadebayo@gmail.com>
2022-01-22 19:49:52 +0100

    create New account method

--- service/accountService.go
+++ service/accountService.go
@@ -2,14 +2,41 @@ package service
 
 import (
 	"github.com/Ad3bay0c/banking/domain/account"
+	"github.com/Ad3bay0c/banking/dto"
 	"github.com/Ad3bay0c/banking/errs"
+	"time"
 )
 
 type AccountService interface {
-	NewAccount() *errs.AppError
+	NewAccount(dto.AccountRequest) (*dto.AccountResponse, *errs.AppError)
 }
 
 type DefaultAccountService struct {
 	repo account.Repository
 }
 
+func (d DefaultAccountService) NewAccount(req dto.AccountRequest) (*dto.AccountResponse, *errs.AppError) {
+	if err := req.Validate(); err != nil {
+		return nil, err
+	}
+	acc := account.Account{
+		ID:          "",
+		CustomerID:  req.CustomerID,
+		OpeningDate: time.Now().Format("2006-01-02 15:04:05"),
+		AccountType: req.AccountType,
+		Amount:      req.Amount,
+		Status:      "1",
+	}
+	newAcc, err := d.repo.Save(acc)
+	if err != nil {
+		return nil, err
+	}
+	response := newAcc.ToNewAccountResponseDto()
+	return &response, nil
+}
+
+func NewAccountService(repo account.Repository) DefaultAccountService {
+	return DefaultAccountService{
+		repo: repo,
+	}
+}
