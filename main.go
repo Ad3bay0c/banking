0a2e672aea3af8e7e63ad90119364af6f00c0180
Ad3bay0c <aclintonadebayo@gmail.com>
2022-01-22 19:49:16 +0100

    removed configuration to server.go file

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"github.com/Ad3bay0c/banking/app"
-	"github.com/Ad3bay0c/banking/domain"
-	"github.com/Ad3bay0c/banking/service"
 	"github.com/joho/godotenv"
 	"log"
 )
@@ -13,9 +11,5 @@ func main() {
 	if err := godotenv.Load(); err != nil {
 		log.Printf("Error reading .env file\n")
 	}
-	db := app.GetDBClient()
-	customerRepository := domain.NewCustomerRepositoryDB(db)
-	//accountRepository := account.NewRepositoryDB(db)
-	ch := &app.CustomerHandlers{Service: service.NewCustomerService(customerRepository)}
-	app.Start(ch)
+	app.Start()
 }
