137ac957f80242a1733b3bf811c8faf37250ddf2
Ad3bay0c <aclintonadebayo@gmail.com>
2022-01-22 19:47:31 +0100

    created a validation metthod to validate amount and account type

--- dto/newAccountRequest.go
+++ dto/newAccountRequest.go
@@ -1,1 +1,22 @@
 package dto
+
+import (
+	"github.com/Ad3bay0c/banking/errs"
+	"strings"
+)
+
+type AccountRequest struct {
+	CustomerID  string  `json:"customer_id"`
+	AccountType string  `json:"account_type"`
+	Amount      float64 `json:"amount"`
+}
+
+func (acc AccountRequest) Validate() *errs.AppError {
+	if acc.Amount < 5000 {
+		return errs.NewValidationError("Amount should be greater than 5000")
+	}
+	if strings.ToLower(acc.AccountType) != "saving" && strings.ToLower(acc.AccountType) != "current" {
+		return errs.NewValidationError("Account type should be either saving or current")
+	}
+	return nil
+}
