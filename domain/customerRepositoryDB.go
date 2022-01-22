10383d038272c3e0cd9f612ff21c6f671766fc4c
Ad3bay0c <aclintonadebayo@gmail.com>
2022-01-22 10:50:58 +0100

    Go formatter applied

--- domain/customerRepositoryDB.go
+++ domain/customerRepositoryDB.go
@@ -8,40 +8,23 @@ import (
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"log"
-	"os"
-	"time"
 )
 
 const (
 	TABLE = "customers"
 )
+
 type CustomerRepositoryDb struct {
 	db *sqlx.DB
 }
-func NewCustomerRepositoryDB() *CustomerRepositoryDb {
-	dns := fmt.Sprintf("host=%s port=%s user=%s password=%s  dbname=%s sslmode=disable",
-		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_USER"), os.Getenv("DB_NAME"))
-	db, err := sqlx.Open("postgres", dns)
 
-
-	if err != nil {
-		panic(err)
-	}
-	if err = db.Ping(); err != nil {
-		db.Close()
-		panic(err)
-	}
-	db.SetConnMaxLifetime(3 * time.Minute)
-	db.SetMaxOpenConns(10)
-	db.SetMaxIdleConns(10)
-
-	log.Println("Database Connected")
-
+func NewCustomerRepositoryDB(DbClient *sqlx.DB) *CustomerRepositoryDb {
 	return &CustomerRepositoryDb{
-		db: db,
+		db: DbClient,
 	}
 }
-func (c *CustomerRepositoryDb) FindAll(status string) ([]Customer, *errs.AppError)  {
+
+func (c *CustomerRepositoryDb) FindAll(status string) ([]Customer, *errs.AppError) {
 	var customers []Customer
 	var query string
 	if status == "1" {
@@ -58,7 +41,7 @@ func (c *CustomerRepositoryDb) FindAll(status string) ([]Customer, *errs.AppErro
 		log.Print(err.Error())
 		if err == sql.ErrNoRows {
 			return nil, errs.NewNotFoundError("customer not found")
-		}else {
+		} else {
 			return nil, errs.NewUnexpectedError("unexpected database error")
 		}
 	}
@@ -70,12 +53,12 @@ func (c *CustomerRepositoryDb) ByID(id string) (*Customer, *errs.AppError) {
 	query := fmt.Sprintf("SELECT customer_id, name, city, zipcode, date_of_birth, status FROM %s WHERE customer_id = $1", TABLE)
 	err := c.db.Get(&customer, query, id)
 	if err != nil {
-		logger.Error("Error: "+err.Error())
+		logger.Error("Error: " + err.Error())
 		if err == sql.ErrNoRows {
 			return nil, errs.NewNotFoundError("customer not found")
 		} else {
 			return nil, errs.NewUnexpectedError("unexpected database errs")
 		}
-    }
+	}
 	return &customer, nil
 }
